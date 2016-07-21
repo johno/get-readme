a90f709c8c305b5b0fcabd92023526857b809d68
ngryman <ngryman@gmail.com>
2016-07-21 21:52:42 +0200

    Async version

--- test.js
+++ test.js
@@ -1,17 +1,17 @@
 import test from 'ava'
 import getReadme from './'
 
-test('gets a readme', t => {
+test('gets a readme', async t => {
   t.plan(2)
 
-  t.is(getReadme('fixtures'), 'Readme contents!\n')
-  t.is(getReadme('fixtures/foo'), 'Foo readme contents!\n')
+  t.is(await getReadme('fixtures'), 'Readme contents!\n')
+  t.is(await getReadme('fixtures/foo'), 'Foo readme contents!\n')
 })
 
-test('gets a readme from a relative dir', t => {
-  t.is(getReadme('../get-readme/fixtures/foo'), 'Foo readme contents!\n')
+test('gets a readme from a relative dir', async t => {
+  t.is(await getReadme('../get-readme/fixtures/foo'), 'Foo readme contents!\n')
 })
 
-test('returns null when no readme exists', t => {
-  t.falsy(getReadme('fixtures/no-readme'))
+test('returns null when no readme exists', async t => {
+  t.falsy(await getReadme('fixtures/no-readme'))
 })
